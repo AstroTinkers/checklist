644da09e62038a5cd09760d51e979a8870c59aeb
Administrator <astrikol@gmail.com>
2022-01-21 16:42:40 +0200

    a

--- set_check.py
+++ set_check.py
@@ -6,8 +6,8 @@ while True:
         if len(check_set) == 0:
             break
         else:
-            final_list = sorted(check_set, key=lambda idc: (idc.isnumeric(), int(idc) if idc.isnumeric() else idc))
-            # idc - input data check - checks every entry if it's integer or strings and sorts accordingly
+            final_list = sorted(check_set, key=lambda item: (item.isnumeric(), int(item) if item.isnumeric() else item))
+            # item - checks every entry if it's integer or strings and sorts accordingly
             file_name = input("Please enter name for your checklist:") or "Checklist"
             timestamp = datetime.datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
             file_name = f"{file_name}_{timestamp}.txt"  # adds name and timestamp
